a8d2b30ba0e502825eca5abdcbc3f4fff0ffcb49
novalfh <annaufal1521@gmail.com>
2026-09-25 02:08:31 +0800

    tabbar

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -4,7 +4,14 @@ import { Tabs } from "expo-router";
 
 export default function TabsLayout () {
     return (
-        <Tabs screenOptions={{headerShown:false, tabBarShowLabel:false}}>
+        <Tabs screenOptions={{
+            headerShown:false,
+            tabBarShowLabel:false,
+            tabBarStyle:{
+                backgroundColor:'rgb(0,0,0)'
+            }
+
+        }} >
             <Tabs.Screen name="index" options={{title:'Home', tabBarIcon: ({color, size}) => {return <Feather name="home" size={size} color={color} />}}}/>
         </Tabs>
     )
